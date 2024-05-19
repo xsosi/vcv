1b76a8a85be6ab2ba47e5c19da179261f045063a
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-20 00:36:50 +0300

    Update database.py

--- AnonXMusic/utils/database.py
+++ AnonXMusic/utils/database.py
@@ -181,6 +181,31 @@ async def del_welcome(chat_id: int):
     return await welcomedb.delete_one({"chat_id": chat_id})
 
 
+async def is_skipmode(chat_id: int) -> bool:
+    mode = skipmode.get(chat_id)
+    if not mode:
+        user = await skipdb.find_one({"chat_id": chat_id})
+        if not user:
+            skipmode[chat_id] = True
+            return True
+        skipmode[chat_id] = False
+        return False
+    return mode
+
+
+async def skip_on(chat_id: int):
+    skipmode[chat_id] = True
+    user = await skipdb.find_one({"chat_id": chat_id})
+    if user:
+        return await skipdb.delete_one({"chat_id": chat_id})
+
+
+async def skip_off(chat_id: int):
+    skipmode[chat_id] = False
+    user = await skipdb.find_one({"chat_id": chat_id})
+    if not user:
+        return await skipdb.insert_one({"chat_id": chat_id})
+
 
 async def get_upvote_count(chat_id: int) -> int:
     mode = count.get(chat_id)
