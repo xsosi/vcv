93756f1c0f8c1a792aad894d8d6bfa6cbdbf4a21
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-20 04:37:33 +0300

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -746,7 +746,7 @@ from AnonXMusic.core.call import Anony
 from AnonXMusic.misc import db
 from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
 from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, queuemarkup, close_markup, stream_markup, stream_markup2, panel_markup_4
+from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup, stream_markup2, panel_markup_4
 from AnonXMusic.utils.pastebin import AnonyBin
 from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
 from youtubesearchpython.__future__ import VideosSearch
